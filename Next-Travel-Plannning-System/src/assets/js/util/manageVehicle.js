66d32adbc508f38d80318ede35dca2a681124cc0
Ashendhiyan <ashen13516@gmail.com>
2023-10-30 09:23:46 -0800

    added save function to manageVehicle.js

---
+++ Next-Travel-Plannning-System/src/assets/js/util/manageVehicle.js
@@ -0,0 +1,37 @@
+
+function save(){
+    let registrationNumber = $('#registrationNumber').val();
+    let vehicleBrand = $('#vehicleBrand').val();
+    let category = $('#category').val();
+    let fuelType = $('#fuelType').val();
+    let isHybrid = $('#isHybrid').val();
+    let fuelUsage = $('#fuelUsage').val();
+    let frontView = $('#frontView').val();
+    let rearView = $('#rearView').val();
+    let sideView = $('#sideView').val();
+    let otherSideView = $('#otherSideView').val();
+    let seatCapacity = $('#seatCapacity').val();
+    let vehicleType = $('#vehicleType').val();
+    let transmission = $('#transmission').val();
+    let driverName = $('#driverName').val();
+    let driverNumber = $('#driverName').val();
+    let licenseFront = $('#licenseFront').val();
+    let licenseRear = $('#licenseRear').val();
+
+    $.ajax({
+        type:"POST",
+        url:"http://localhost:8080/vehicleService/api/v1/vehicle",
+        data:{registrationNumber:registrationNumber, vehicleBrand:vehicleBrand, category:category, fuelType:fuelType,
+        isHybrid:isHybrid, fuelUsage:fuelUsage, frontView:frontView, rearView:rearView, sideView:sideView,
+            otherSideView:otherSideView, seatCapacity:seatCapacity, vehicleType:vehicleType, transmission:transmission,
+            driverName:driverName, driverNumber:driverNumber, licenseFront:licenseFront, licenseRear:licenseRear
+        },
+        success:(response) => {
+            console.log(response)
+        },
+        error:(error) => {
+            console.log(error)
+        }
+    })
+}
+
